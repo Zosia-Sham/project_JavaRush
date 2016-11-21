c37121edbc4be614b2ff685a8936383fe3618517
Zosia-Sham <shamina_z@list.ru>
2016-11-21 23:48:15 +0300

    Create UserController.java

---
+++ src/main/java/usermanager/controller/UserController.java
@@ -0,0 +1,37 @@
+package usermanager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import usermanager.model.User;
+import usermanager.service.UserService;
+
+@Controller
+public class UserController {
+    private static String stringForSearch;
+    private UserService userService;
+    @Autowired(required = true)
+    @Qualifier(value = "userService")
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String listUsers(Model model){
+        model.addAttribute("user", new User());
+        model.addAttribute("listUsers", this.userService.getListUsers());
+        return "users";
+    }
+
+    @RequestMapping(value = "/users/add", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute("user") User user){
+        if(user.getId() == 0){
+            this.userService.addUser(user);
+        }else {
+            this.userService.updateUser(user);
+        }
+        return "redirect:/users";
+    }
+
